bc0d524e57874ba2a81b0dcffeb4332833184620
Harsh Kukadiya <harsh.kukadiya.101@gmail.com>
2025-01-15 15:06:00 +0530

    Complete Day 1 part 1 and 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,36 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = input.map { inputPair ->
+            val first = inputPair.substringBefore(" ").toInt()
+            val second = inputPair.substringAfterLast(" ").toInt()
+            first to second
+        }.unzip()
+
+        val result = left.sorted().zip(right.sorted()).map { (first,second) ->
+            abs(first - second)
+        }.sum()
+
+        return result
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val (left, right) = input.map { inputPair ->
+            inputPair.split(Regex("\\s+")).let {
+                it[0].toLong() to it[1].toLong()
+            }
+        }.unzip()
+
+        val frequency = right.groupingBy { it }.eachCount()
+
+        val result = left.fold(0L){ acc, num ->
+            acc + num * frequency.getOrDefault(num ,0)
+        }
+
+        return result
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
